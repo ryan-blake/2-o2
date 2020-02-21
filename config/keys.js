ba10ede69f968e1d49a8f1ff249931ff59ed3ae7
ryan-blake <ryan_blake@icloud.com>
2020-02-21 16:14:49 -0600

    sd

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: "mongodb+srv://b-1b:bb11bb@cluster0-e00zd.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret",
+  TwitKey:"F1GLCC0DNU60XwjvSWmtSWoGf",
+  TwitSecret:"5ug8spISR4ZgfryuS12yj2x5vnGIVi9EH2c6O2t0CF79WIox1G"
+};
